55b6e09ef676967e4a0a7ad13d58301dfa270d08
Yutong0928 <97705964+Yutong0928@users.noreply.github.com>
2022-06-13 16:25:48 -0700

    update

---
+++ frontEnd/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import SearchBar from "./SearchBar";
+
+function Home(props) {
+  return (
+    <div className="home">
+      <SearchBar />
+    </div>
+  );
+}
+
+export default Home;
